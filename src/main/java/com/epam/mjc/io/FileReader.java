dc198f6b5d537c46a6d04299a70306a190bec345
Shushoook <shushoook666@gmail.com>
2022-12-21 22:06:45 +0400

    stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,23 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String[] params = new String[4];
+        int counter=0;
+        try ( BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+            String str;
+            while ((str = bufferedReader.readLine())!=null){
+                params[counter++]=  str.split(":")[1].trim();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
+        return new Profile(params[0],Integer.parseInt(params[1]),params[2],Long.parseLong(params[3]));
     }
 }
